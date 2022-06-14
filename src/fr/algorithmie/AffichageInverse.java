af19d8b325820f7ec7ff71acbbbdee3344891f85
hanah <hernandez.hana@gmail.com>
2022-06-14 09:28:07 +0000

    TP 07 AfficageInverse
    Signed-off-by: hanah <hernandez.hana@gmail.com>

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -4,31 +4,45 @@ public class AffichageInverse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		// Soit le tableau suivant : int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-		int[] tableauArray = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-		
+
+		// Soit le tableau suivant : int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7,
+		// -1, 17, 2, 3, 0, 14, -4} ;
+		int[] tableauArray = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
 		// Déclaration d'une variable stockant la longueur du tableau (facultatif)
-				int longtab = tableauArray.length;
-		
+		int longtab = tableauArray.length;
+
 		// Afficher l’ensemble des éléments du tableau grâce à une boucle
 		String arrayText = "l’ensemble des éléments du tableau: ";
 		System.out.println(arrayText);
-		System.out.println("-------------------------------------");		
+		System.out.println("-------------------------------------");
 		for (int i = 0; i < longtab; i++) {
 			System.out.println(tableauArray[i]);
 		}
 		System.out.println();
-		
+
 		// Afficher l’ensemble des éléments dans l’ordre inverse du tableau
 		String tableauInverse = "l’ensemble des éléments du tableau dans l’ordre inverse sont: ";
 		System.out.println(tableauInverse);
-		System.out.println("-------------------------------------");		
-		for (int i = longtab - 1; i >= 0;i--) {
+		System.out.println("-------------------------------------");
+		for (int i = longtab - 1; i >= 0; i--) {
 			System.out.println(tableauArray[i]);
 		}
+		System.out.println();
 
-		
+		// Créer un tableau arrayCopy et copier tous les éléments de array dans
+		// arrayCopy
+		String newTableau = "l’ensemble des éléments du tableau arrayCopy ";
+		System.out.println(newTableau);
+		System.out.println("-------------------------------------");
+		int[] arrayCopy = new int[tableauArray.length];
+		for (int i = 0; i < longtab; i++) {
+			arrayCopy[i] = tableauArray[i];
+			// Afficher arrayCopy tableau
+
+			System.out.println(arrayCopy[i]);
+		}
+
 	}
 
 }
